4d453ee1f24b6002a1117a1d0ae6d7aa41196d55
Oda <oda@192.168.1.253>
2025-02-10 13:25:32 +0000

    checksum validator works
    checked @ https://skilldrick.github.io/easy6502/simulator.html

---
+++ checksum/validate-checksum.asm
@@ -0,0 +1,52 @@
+; store bytes to validate 
+LDA #$06
+STA $00
+
+LDA #$05
+STA $01
+
+LDA #$00
+STA $02
+
+LDA #$02
+STA $03
+
+LDA #$F3 ; checksum
+STA $04
+
+; storing the address of the data array in $10-11
+LDA #$00
+STA $10
+STA $11  ; $1011 now points to $0000
+
+LDX #$05 ; array length
+
+; copy array length to decrement
+TXA
+TAY
+
+; set up the loop
+LDA #$00
+DEY
+
+; start looping
+LOOP:
+CLC
+ADC ($10), Y
+DEY
+BPL LOOP
+
+; A now holds the data + checksum. If checksum was 
+; valid, A holds #$00
+CMP #$00
+BNE ERROR
+
+; here sum is 0, Z=1 and checksum is valid 
+
+LDA #$01
+JMP DONE
+
+ERROR:
+LDA #$00
+
+DONE:
